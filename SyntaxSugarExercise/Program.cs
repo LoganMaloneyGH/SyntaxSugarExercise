df32317ed40fdf391315c2e190205704fc4717e5
logan Maloney <shakenasty13@gmail.com>
2021-11-01 09:52:52 -0500

    initial commit

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 15;
+            
+            var response = (answer < 9) ? $"{answer} is less than nine" :
+                (answer == 15) ? $"{answer} is 15!" :
+                (answer == 50) ? $"{answer} is 50!" :
+                $"{answer} greater than or equal to nine";
+
+            Console.WriteLine(response); 
+
+          
+        }
+    }
+}
